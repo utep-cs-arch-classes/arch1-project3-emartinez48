1a52203cb9b1cab644010c52dec4167b644d079a
Fuzzion XYZ <parra.justin.main@gmail.com>
2016-11-29 20:46:59 -0700

    added killer boxes

--- video-game/main.c
+++ video-game/main.c
@@ -8,6 +8,7 @@
  */
 #include <msp430.h>
 #include <stdjp.h>
+#include <stdlib.h> 		/* for malloc */
 #include <libTimer.h>
 #include <lcdutils.h>
 #include <lcddraw.h>
@@ -23,37 +24,64 @@ AbRect rect10 = {abRectGetBounds, abRectCheck, {5,5}}; /**< 10x10 rectangle */
 AbRect rect3 = {abRectGetBounds, abRectCheck, {3,3}}; /**< 10x10 rectangle */
 AbRArrow rightArrow = {abRArrowGetBounds, abRArrowCheck, 30};
 
-AbRectOutline topOutline = {	/* playing field */
-  abRectOutlineGetBounds, abRectOutlineCheck,
-  {screenWidth/2 - 10, screenHeight/2}
-};
-
 AbRectOutline fieldOutline = {	/* playing field */
   abRectOutlineGetBounds, abRectOutlineCheck,
   {screenWidth/2 - 10, screenHeight/2 - 10}
 };
 
-Layer topFenceLayer = {		/* playing field as a layer */
-  (AbShape *) &topOutline,
-  {screenWidth/2, screenHeight/2},/**< center */
+Layer eLazer = {
+  (AbShape *)&rect3,
+  {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_BLACK,
+  COLOR_WHITE,
   0
+};
+
+Layer dLazer = {
+  (AbShape *)&rect3,
+  {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_WHITE,
+  &eLazer
+};
+
+Layer cLazer = {
+  (AbShape *)&rect3,
+  {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_WHITE,
+  &dLazer
+};
+
+Layer bLazer = {
+  (AbShape *)&rect3,
+  {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_WHITE,
+  &cLazer
+};
+
+Layer aLazer = {
+  (AbShape *)&rect3,
+  {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
+  {0,0}, {0,0},				    /* last & next pos */
+  COLOR_WHITE,
+  &bLazer
 };
 
 Layer rightLazer = {
   (AbShape *)&rect3,
   {(screenWidth-5), (screenHeight/2)}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
-  &topFenceLayer
+  COLOR_WHITE,
+  &aLazer
 };
 
 Layer topLazer = {
   (AbShape *)&rect3,
   {(screenWidth/2), (5)}, /**< bit below & right of center */
   {0,0}, {0,0},				    /* last & next pos */
-  COLOR_PINK,
+  COLOR_WHITE,
   &rightLazer
 };
 
@@ -85,9 +113,15 @@ typedef struct MovLayer_s {
 
 /*
   Top Arrow
+  topBeam
  */
-MovLayer m1lazer = { &rightLazer, {0,1}, 0 };
-MovLayer mlazer = { &topLazer, {1,0}, &m1lazer };
+MovLayer elazer = { &eLazer, {0,1}, 0 };
+MovLayer dlazer = { &dLazer, {0,1}, 0 };
+MovLayer clazer = { &cLazer, {0,1}, 0 };
+MovLayer blazer = { &bLazer, {0,1}, 0 };
+MovLayer alazer = { &aLazer, {2,1}, 0 };
+MovLayer m1lazer = { &rightLazer, {0,1}, &alazer };
+MovLayer mlazer = { &topLazer, {1,1}, &m1lazer };
 
 /* initial value of {0,0} will be overwritten */
 MovLayer ml0 = { &layer0, {0,0}, &mlazer };
@@ -99,7 +133,78 @@ MovLayer ml0 = { &layer0, {0,0}, &mlazer };
 
 
 
-movLayerDraw(MovLayer *movLayers, Layer *layers)
+
+
+
+
+
+
+
+
+
+
+//
+typedef struct killers_s {
+  MovLayer *mov;
+  int timer;
+  int halfTimer;
+  int forthTimer;
+  char killMode;
+  struct killers_s *next;
+} killer;
+
+killer e = { &elazer, 0,0,0,0, 0};
+killer d = { &dlazer, 0,0,0,0, 0};
+killer c = { &clazer, 0,0,0,0, 0};
+killer b = { &blazer, 0,0,0,0, 0};
+killer a = { &alazer, 0,0,0,0, 0};
+killer r = { &m1lazer, 0,0,0,0, &a};
+killer t = { &mlazer, 0,0,0,0, &r};
+// void movLayerAdd(MovLayer *ml, MovLayer *new) {
+//   if( ml->next != 0 )
+//     movLayerAdd(ml->next, new);
+//   else {
+//     ml->next = new;
+//     return;
+//   }
+// }
+// void kLayerAdd(killer *ml, killer *new) {
+//   for( ; ml->next != 0; ml = ml->next ){}
+//   ml->next = new;
+// }
+// void addKiller() {
+//   Layer *temp = (Layer *) malloc( sizeof(Layer) );
+//   temp->abShape = (AbShape *)&rect3;
+//   temp->pos.axes[0] = (screenWidth/2 + (random() % (screenWidth) - screenWidth/2));
+//   temp->pos.axes[1] = (screenHeight/2 + (random() % (screenHeight) - screenHeight/2));
+//   temp->posLast.axes[0] = temp->pos.axes[0];
+//   temp->posLast.axes[1] = temp->pos.axes[1];
+//   temp->posNext.axes[0] = 0;
+//   temp->posNext.axes[1] = 0;
+//   temp->color = COLOR_WHITE;
+//   temp->next = 0;
+//   MovLayer *temp_ml = (MovLayer *) malloc( sizeof(MovLayer) );
+//
+//   temp_ml->layer = temp;
+//   temp_ml->velocity.axes[0] = random() % 6 + 1;
+//   temp_ml->velocity.axes[1] = random() % 6 + 1;
+//   temp_ml->next = 0;
+//
+//   killer *temp_k = (killer *) malloc( sizeof(killer) );
+//
+//   movLayerAdd(&ml0,temp_ml);
+//   temp_k->mov = temp_ml;
+//   temp_k->timer = ((random() % 10) + 1) * 250;
+//   temp_k->halfTimer = temp_k->timer /2;
+//   temp_k->forthTimer = temp_k->halfTimer /2;
+//   temp_k->killMode = 0;
+//   temp_k->next = 0;
+//
+//   // initKiller(temp_k, temp_ml);
+//   // kLayerAdd(&a,temp_k);
+// }
+
+void movLayerDraw(MovLayer *movLayers, Layer *layers)
 {
   int row, col;
   MovLayer *movLayer;
@@ -211,11 +316,6 @@ Region topFence;
 
 
 
-u_int topCount = 0;
-u_int rightCount = 0;
-u_int topCount_half = 0;
-u_int rightCount_half = 0;
-
 
 
 /** Initializes everything, enables interrupts and green LED,
@@ -234,17 +334,12 @@ void main()
 
   layerInit(&layer0);
   layerDraw(&layer0);
+  // addKiller();
 
 
   layerGetBounds(&fieldLayer, &fieldFence);
-  layerGetBounds(&topFenceLayer, &topFence);
+  // layerGetBounds(&topFenceLayer, &topFence);
 
-  topCount = random() % 6 + 1;
-  topCount *= 250;
-  topCount_half = topCount/2;
-  rightCount = random() % 6 + 1;
-  rightCount *= 250;
-  rightCount_half = rightCount/2;
 
   enableWDTInterrupts();      /**< enable periodic interrupt */
   or_sr(0x8);	              /**< GIE (enable interrupts) */
@@ -260,6 +355,7 @@ void main()
     movLayerDraw(&ml0, &layer0);
   }
 }
+unsigned long score = 0;
 
 /** Watchdog timer interrupt handler. 15 interrupts/sec */
 void wdt_c_handler()
@@ -267,25 +363,13 @@ void wdt_c_handler()
   static short count = 0;
   P1OUT |= GREEN_LED;		      /**< Green LED on when cpu on */
   count ++;
-  if( topCount > 0)
-    topCount -= 1;
-  if(topCount >= topCount_half) {
-    (&topLazer)->color = COLOR_WHITE;
-  } else if(topCount < topCount_half && topCount >= topCount_half/2) {
-    (&topLazer)->color = COLOR_YELLOW;
-  } else if(topCount < topCount_half/2) {
-    (&topLazer)->color = COLOR_RED;
-    (&mlazer)->velocity.axes[0] = 0;
-    (&mlazer)->velocity.axes[1] = 0;
-  }
-
   if (count == 10) {
+    count = 0;
     redrawScreen = 1;
 
     char buffer [33];
-    itoa (topCount,buffer);
+    itoa (score,buffer);
     buffer[32] = '\0';
-
 
     drawString5x7(0,0, buffer, COLOR_GREEN, COLOR_RED);
 
@@ -308,7 +392,6 @@ void wdt_c_handler()
     // mlAdvance(&mlazer, &topFence);
     // if (!sw1down)
     //   redrawScreen = 1;
-    count = 0;
   }
   P1OUT &= ~GREEN_LED;		    /**< Green LED off when cpu off */
 }
